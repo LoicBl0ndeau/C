ca64a3785288461c01d49c45490d1c3bf7adcb74
ReziT <loicblondeau59@gmail.com>
2021-12-16 23:08:16 +0100

    fin

--- Exo3/main.c
+++ Exo3/main.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct Single_coefficient {
 	int coeff; // coefficient du polynôme
 	struct Single_coefficient* next; // pointeur vers le maillon suivant
 } Single_coefficient;
 
-typedef struct SingleLinkedListElem {
-	Single_coefficient* data;
-} SingleLinkedListElem;
-
 typedef struct LinkedListCoefficients {
-	SingleLinkedListElem* head; // pointeur vers la tête de liste
+	Single_coefficient* head; // pointeur vers la tête de liste
 	int size;
-} LinkedListCoefficients;void add_an_coefficient(LinkedListCoefficients* liste, int coefficient);LinkedListCoefficients* create_liste();int main() {	LinkedListCoefficients* liste = create_liste();	add_an_coefficient(liste, 2);
+} LinkedListCoefficients;void add_an_coefficient(LinkedListCoefficients* liste, int coefficient);LinkedListCoefficients* create_liste();int calculate_polynome(LinkedListCoefficients* liste, int x);int main() {	LinkedListCoefficients* liste = create_liste();	add_an_coefficient(liste, 2);
 	add_an_coefficient(liste, 5);
 	add_an_coefficient(liste, -3);
-	add_an_coefficient(liste, 8);	return EXIT_SUCCESS;}LinkedListCoefficients* create_liste() {	LinkedListCoefficients* liste;	liste->head = NULL;	liste->size = 0;	return liste;}void add_an_coefficient(LinkedListCoefficients* liste, int coefficient) {	Single_coefficient* tmp = (Single_coefficient*)malloc(sizeof(Single_coefficient));	if (tmp != NULL) {		SingleLinkedListElem* tail = liste->head;		for (int i = 0; i < liste->size; i++)
+	add_an_coefficient(liste, 8);	int test = calculate_polynome(liste, 0);	printf("%d",test);	return EXIT_SUCCESS;}LinkedListCoefficients* create_liste() {	LinkedListCoefficients* liste = (LinkedListCoefficients*)malloc(sizeof(LinkedListCoefficients));	if (liste != NULL) {		liste->head = NULL;		liste->size = 0;	}	return liste;}void add_an_coefficient(LinkedListCoefficients* liste, int coefficient) {	Single_coefficient* tmp = (Single_coefficient*)malloc(sizeof(Single_coefficient));	if (tmp != NULL) {		if (liste->size == 0) {			liste->head = tmp;		}		else {			Single_coefficient* tail = liste->head;			for (int i = 0; i < liste->size; i++)
+			{
+				tail = tail->next;
+			}			tmp->coeff = coefficient;			tmp->next = NULL;			tail->next = tmp;		}	}}int calculate_polynome(LinkedListCoefficients* liste, int x) {	Single_coefficient* tmp = (Single_coefficient*)malloc(sizeof(Single_coefficient));	if (tmp != NULL && liste->head != NULL) {		tmp = liste->head;		int somme = tmp->coeff;		for (int i = 1; i < liste->size; i++)
 		{
-			tail = tail->data->next;
-		}		tail->data->next = tmp;		tmp->coeff = coefficient;	}}
+			tmp = tmp->next;
+			somme += tmp->coeff * pow(x, i);
+		}		return somme;	}	else {		return -1;	}}
